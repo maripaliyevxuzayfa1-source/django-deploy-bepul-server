8fd63154c1bcef620104b02e5865ce04a170d0a1
RAmz <ramziddinyuldash23@gmail.com>
2026-02-25 20:53:39 +0500

    seks

---
+++ salom/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import TestUchun
+# Register your models here.
+
+admin.site.register(TestUchun)
---
+++ salom/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class TestUchun(models.Model):
+    name = models.CharField(max_length=255)
+    
+    
+    def __str__(self):
+        return self.name
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
